5433f07a8783a5a1ba697ab110a6458b4b80d984
James Lucas <svix-james@users.noreply.github.com>
2024-01-05 16:37:54 -0600

    Add documentation placeholders / fix clippy errors

--- src/lib.rs
+++ src/lib.rs
@@ -636,6 +636,7 @@ mod macros;
 /// Baseline OpenID Connect implementation and types.
 pub mod core;
 
+/// FIXME: documentation
 pub mod jwt_profile;
 
 /// OpenID Connect Dynamic Client Registration.
